a5374122d1c0dce37a13b8c7a107ecfe362e07d9
Kamaal Farah <kamaal.f1@gmail.com>
2019-10-24 19:28:50 +0200

    Displaying session name and candidate names

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 
 import AppNavigator from './AppNavigator';
-import { SessionProvider } from './Contexts/sessionContext';
 
 import './styles/css/App.css';
 
 const App: React.FC = (): JSX.Element => {
-  return (
-    <SessionProvider>
-      <AppNavigator />
-    </SessionProvider>
-  );
+  return <AppNavigator />;
 };
 
 export default App;
--- src/Pages/Session/index.tsx
+++ src/Pages/Session/index.tsx
@@ -1,22 +1,45 @@
-import React, { useContext, useEffect } from 'react';
-
-import { SessionContext } from '../../Contexts/sessionContext';
+import React, { useEffect, useState } from 'react';
 
 const Session: React.ElementType = ({ match }): JSX.Element => {
-  const sessionContext = useContext(SessionContext);
-
-  useEffect(() => {
-    fetch(`http://localhost:8000/sessions/${match.params.id}`)
-      .then(results => results.json())
-      .then(json => console.log(json))
-      .catch(err => console.error(err));
-  }, [match.params.id]);
-
-  if (Array.isArray(sessionContext)) {
-    // const [session, setSession] = sessionContext;
+  interface CandidateType {
+    name: String;
+  }
+  interface SessionType {
+    name: String;
+    candidates: CandidateType[];
   }
 
-  return <p>Session</p>;
+  const [sessionData, setSessionData] = useState<null | SessionType>(null);
+  const [errors, setErrors] = useState({
+    session: null,
+  });
+
+  useEffect(() => {
+    if (errors !== null) {
+      fetch(`http://localhost:8000/sessions/${match.params.id}`)
+        .then(results => results.json())
+        .then(json => setSessionData(json))
+        .catch(err =>
+          setErrors({
+            ...errors,
+            session: err,
+          }),
+        );
+    }
+  }, [match.params.id, errors]);
+
+  console.log('sessionData', sessionData);
+
+  return sessionData === null ? (
+    <h1>LOADING....</h1>
+  ) : (
+    <>
+      <h1>{sessionData.name}</h1>
+      {sessionData.candidates.map(({ name }, index) => (
+        <p key={index}> {name}</p>
+      ))}
+    </>
+  );
 };
 
 export default Session;
